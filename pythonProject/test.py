f9b84b321495e1524261938da77d6d31b5697a6d
GYH <3193675178@qq,com>
2024-06-22 23:52:46 +0800

    基础语法学习

--- pythonProject/test.py
+++ pythonProject/test.py
@@ -3,9 +3,13 @@ print("hello world!")
 """
 
 """
+"""
+格式化输出
+"""
+name = "小王"
+age = 30
+print("他的名字叫%s,今年%d岁" % (name, age))
 
-
-def function():
-    return 11
-def function2():
-    return 13
+# format格式化
+print(f"姓名:{name},年龄:{age}")
+print("%.2f" % 12.111)
