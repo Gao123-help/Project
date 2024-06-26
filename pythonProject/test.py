6b3bcafbecc4910ac4aef4696b2547682ec343ba
GYH <3193675178@qq,com>
2024-06-26 23:00:07 +0800

    基础语法学习

--- pythonProject/test.py
+++ pythonProject/test.py
@@ -6,10 +6,28 @@ print("hello world!")
 """
 格式化输出
 """
-name = "小王"
-age = 30
-print("他的名字叫%s,今年%d岁" % (name, age))
+# name = "小王"
+# age = 30
+# print("他的名字叫%s,今年%d岁" % (name, age))
+#
+# # format格式化
+# print(f"姓名:{name},年龄:{age}")
+# print("%.2f" % 12.111)
+#
+# tmp = "30"
+# print("%d" % int(tmp))
 
-# format格式化
-print(f"姓名:{name},年龄:{age}")
-print("%.2f" % 12.111)
+sTmp = "[1,2,3]"
+print(type(eval(sTmp)))
+
+print(f"{9 / 2}")
+# 短路运算
+print(3 and 4)
+# 三目运算符
+a = 10 if 10 > 20 else 20
+# 字符串拼接
+slist = {"python", "linux", "C++"}
+print("-".join(slist))
+# 首字母大写：str.title()
+# 字符串的判断语句：startwith()\endwith()
+
